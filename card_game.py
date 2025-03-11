2a29960d73773e41e17ed319a87fbc045c868e61
Daniels Veinbergs <166484832+24DP4DVein@users.noreply.github.com>
2025-03-11 08:35:22 +0000

    two dealer card

--- card_game.py
+++ card_game.py
@@ -1,9 +1,15 @@
 
-masivs_lietotai = []
+from random import randint
 
-def lietotaja_registracija():
-    print("Ievadi savo Vadru:")
-    masivs_lietotai = input("")
-    
 
+suits = ['♠', '♥', '♦', '♣']
+ranks = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
+def diler():
+    diler_card = []
+    diler_card.append(randint(0,10))
+    diler_card.append(randint(0,10))
+    print(diler_card)
+
+diler()
 
